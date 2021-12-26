9029f06056c7665942be619ec91d95c411011548
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-26 21:10:47 +0100

    remove duplicate client code

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,3 @@ require("dotenv").config();
 const client = new ExtendedClient([Intents.FLAGS.GUILDS]);
 
 client.start();
-
-client.once("ready", () => {
-    console.log("Ready!");
-});
-
-client.login(process.env.botToken);
