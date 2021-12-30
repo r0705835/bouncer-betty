db9c1bb4b1fa3b8b1bbc28b3ac36b3c05d05b76f
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-30 18:00:41 +0100

    create announcement message with button

--- src/commands/announcement.ts
+++ src/commands/announcement.ts
@@ -1,13 +1,23 @@
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, MessageActionRow, MessageButton } from "discord.js";
 import { CommandInt } from "../model/CommandInt";
 
 export const announcement: CommandInt = {
     name: "announcement",
     description: "Sends an embed mesage with an interaction button.",
     run: async (interaction: CommandInteraction) => {
+        const button = new MessageButton()
+            .setCustomId("announcement_toggle")
+            .setLabel("Toggle announcement role")
+            .setStyle("PRIMARY");
+        const actionRow = new MessageActionRow();
+        actionRow.addComponents(button);
+        interaction.channel.send({
+            content: "test",
+            components: [actionRow]
+        });
         interaction.reply({
             content: "pong!",
             ephemeral: true
-        })
+        });
     }
 }
