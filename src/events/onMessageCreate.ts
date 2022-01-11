7620c4339af3a3fcb3b2944045ac73b25c1c3b22
r0705835 <32357167+r0705835@users.noreply.github.com>
2022-01-11 17:21:12 +0100

    add checks for bot users

--- src/events/onMessageCreate.ts
+++ src/events/onMessageCreate.ts
@@ -2,5 +2,6 @@ import { Message } from "discord.js";
 import { manageMemberData } from "../database/manageMemberData";
 
 export const onMessageCreate = async (message: Message) => {
+    if(message.author.bot) { return; }
     manageMemberData(message.member);
 }
