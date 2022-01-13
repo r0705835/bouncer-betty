1424f5d85cdc010e218e335c190e1677d0f78dee
r0705835 <32357167+r0705835@users.noreply.github.com>
2022-01-13 18:43:05 +0100

    update logging for onUserUpdate to show tag

--- src/events/onUserUpdate.ts
+++ src/events/onUserUpdate.ts
@@ -2,7 +2,7 @@ import { PartialUser, User } from "discord.js";
 import { updateUser } from "../database/updateUser";
 
 export async function onUserUpdate(oldUser: User | PartialUser, newUser: User) {
-    console.log("a user has changed their info new name is" + newUser.username );
+    console.log("a user has changed their info. Their new tag is " + newUser.tag );
     if(oldUser.partial) {
         oldUser = await oldUser.fetch();
     }
