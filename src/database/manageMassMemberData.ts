104a774fc1414685d307e96778b2bf262aaeacc3
r0705835 <32357167+r0705835@users.noreply.github.com>
2022-01-13 17:04:21 +0100

    check if users are also present in database before adding new ones

--- src/database/manageMassMemberData.ts
+++ src/database/manageMassMemberData.ts
@@ -1,23 +1,31 @@
-import { Collection, GuildMember } from "discord.js"
+import { Collection, GuildMember } from "discord.js";
+import { getMemberData } from "./getMemberData";
 import MemberModel from "./models/MemberModel";
 
-export async function manageMassMemberData(members: Collection<string, GuildMember>){
+export async function manageMassMemberData(members: Collection<string, GuildMember>) {
     const membersToAdd = [];
     const lastActivity = Date.now();
+    const guildId = members.first().guild.id;
+    const presentMembers = (await getMemberData(guildId)).map(presentMember => {
+        return presentMember.discordId;
+    });
     members.forEach(member => {
-        const memberToAdd = new MemberModel({
-            discordId: member.id,
-            guildId: member.guild.id,
-            username: member.user.username,
-            tag: member.user.tag,
-            lastActivity: lastActivity,
-        });
-        membersToAdd.push(memberToAdd);
+        if (!presentMembers.includes(member.id)) {
+            const memberToAdd = new MemberModel({
+                discordId: member.id,
+                guildId: member.guild.id,
+                username: member.user.username,
+                tag: member.user.tag,
+                lastActivity: lastActivity,
+            });
+            membersToAdd.push(memberToAdd);
+        };
+
     });
     try {
         await MemberModel.insertMany(membersToAdd);
         console.log("Added users from guild " + members.first().guild.name + " to the database.");
-    } catch(error) {
+    } catch (error) {
         console.log("Something went wrong adding users to the database.");
     }
 }
